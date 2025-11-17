f33d753f6fe40e9afe6f80d6c44c2ee83d96d850
Juan Chapur <juanchapur99@gmail.com>
2025-11-17 17:48:58 -0300

    feat: eliminar abrir Docker Desktop

--- run_process.bat
+++ run_process.bat
@@ -6,9 +6,6 @@ cd C:\desarrollo\youtube-downloader-mp3
 REM Ejecutar el script para llenar los inputs
 python populate_inputs.py
 
-REM Verificar si Docker está corriendo
-powershell -Command "if (-not (Get-Process docker -ErrorAction SilentlyContinue)) { Start-Process 'Docker Desktop' -Wait }"
-
 REM Ejecutar Docker Compose
 powershell -Command "docker-compose up; if ($LASTEXITCODE -ne 0) { docker-compose up --build }"
 
